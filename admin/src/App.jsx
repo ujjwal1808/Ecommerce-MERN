d371b38d3dfd1787e75792670b8099fc01ffcd98
Ujjwal Maurya <88647832+ujjwal1808@users.noreply.github.com>
2024-10-17 23:29:40 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,10 +5,21 @@ import ProductList from './components/ProductList'
 import OrderList from './components/OrderList'
 import AddProduct from './components/AddProduct'
 
-function App() {
+const App = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <Router>
       <div className="flex flex-col md:flex-row">
+        {/* Sidebar */}
+        <div className={`fixed md:static top-0 left-0 h-full w-64 bg-gray-800 transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}>
+          <Sidebar />
+        </div>
+
         {/* Main Content */}
         <div className={`flex-1 p-8 transition-all duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-0'} md:ml-64`}>
           <button onClick={toggleSidebar} className="md:hidden text-white">
@@ -23,7 +34,7 @@ function App() {
         </div>
       </div>
     </Router>
-  )
-}
+  );
+};
 
 export default App

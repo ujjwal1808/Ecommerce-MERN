70efb088fa74cf2335317c887d154edadb9aa474
Ujjwal Maurya <88647832+ujjwal1808@users.noreply.github.com>
2024-10-17 22:57:34 +0530

    responsive Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -20,7 +20,7 @@ import { Link } from 'react-router-dom';
 // Loader component
 const Loader = () => (
   <div className="flex justify-center items-center py-20">
-    <div className="loader border-t-transparent border-solid rounded-full border-orange-500 border-8 h-16 w-16 animate-spin"></div>
+    <div className="loader border-t-transparent border-solid rounded-full border-blue-500 border-8 h-16 w-16 animate-spin"></div>
   </div>
 );
 
@@ -65,79 +65,76 @@ const Home = () => {
   return (
     <>
       <div>
-        <div className="bg-black text-white p-1 w-full text-xs text-center">
+        <div className="bg-black text-white p-2 w-full text-xs text-center">
           Shop Our Sale: Big Savings Await!
         </div>
-        <div className="sm:mx-36">
-          <div className="overflow-hidden relative ">
+
+        {/* Banner Section */}
+        <div className="sm:mx-0 lg:mx-36">
+          <div className="overflow-hidden relative w-full h-52 md:h-80 lg:h-[500px]">
             <div
-              className={`flex transition ease-out duration-40 `}
+              className={`flex transition ease-out duration-500 `}
               style={{
                 transform: `translateX(-${current * 100}%)`,
               }}
             >
-              {slides.map((s) => {
-                return <img src={s} alt="banner" />;
-              })}
+              {slides.map((s, index) => (
+                <img key={index} src={s} alt="banner" className="w-full object-cover" />
+              ))}
             </div>
 
-            <div className="absolute top-0 h-full w-full justify-between items-center flex text-white px-10 text-3xl">
+            <div className="absolute top-0 h-full w-full flex justify-between items-center px-3 lg:px-10 text-white">
               <button onClick={previousSlide}>
-                <span class="material-symbols-outlined text-black font-bold ">
+                <span className="material-symbols-outlined text-black font-bold">
                   arrow_back_ios
                 </span>
               </button>
               <button onClick={nextSlide}>
-                <span class="material-symbols-outlined text-black font-bold ">
+                <span className="material-symbols-outlined text-black font-bold">
                   arrow_forward_ios
                 </span>
               </button>
             </div>
 
-            <div className="absolute bottom-0 py-4 flex w-fit justify-center gap-3 w-full">
-              {slides.map((s, i) => {
-                return (
-                  <div
-                    onClick={() => {
-                      setCurrent(i);
-                    }}
-                    key={"circle" + i}
-                    className={`rounded-full w-2 h-2 cursor-pointer  ${i === current ? 'bg-white' : 'bg-gray-500'
-                      }`}
-                  ></div>
-                );
-              })}
+            <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
+              {slides.map((_, i) => (
+                <div
+                  key={i}
+                  onClick={() => setCurrent(i)}
+                  className={`rounded-full w-2 h-2 cursor-pointer ${i === current ? 'bg-white' : 'bg-gray-500'}`}
+                ></div>
+              ))}
             </div>
           </div>
         </div>
 
         {/* Men's Section */}
-        <div className='text-center'>
-          <h1 className='text-3xl md:text-5xl m-5'>Just Launched!!! 🚀</h1>
-          <p className='mt-6'>Summer Collections for men</p>
+        <div className="text-center mt-8">
+          <h1 className="text-2xl md:text-4xl m-5">Just Launched!!! 🚀</h1>
+          <p className="text-sm md:text-lg mb-6">Summer Collections for Men</p>
 
           {loadingMen ? (
-            <Loader /> // Show loader when data is loading
+            <Loader />
           ) : (
-            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6'>
-              {products.filter((item) => item.category === "men").slice(0, 8).map((product) => (
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mx-4 lg:mx-0">
+              {products.filter((item) => item.category === 'men').slice(0, 8).map((product) => (
                 <div
                   key={product.id}
-                  className='my-5 mx-auto border border-gray-200 rounded-lg text-left shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out w-64 h-80'
+                  className="border border-gray-200 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
                 >
                   <Link to={`${product.name}`} key={product.id}>
-                    <div className='overflow-hidden h-48'>
+                    <div className="overflow-hidden h-48">
                       <img
                         src={product.image}
                         onClick={() => SinglePage(product)}
-                        alt="image"
-                        className='w-full h-full object-cover transform transition-transform duration-500 ease-in-out hover:scale-110 cursor-pointer'
+                        alt={product.name}
+                        className="w-full h-full object-cover transform transition-transform duration-500 ease-in-out hover:scale-110 cursor-pointer"
                       />
                     </div>
                     <div className="p-4">
-                      <div className='font-bold text-lg text-gray-800 truncate'>{product.name}</div>
-                      <div className='mt-1 text-lg text-green-600 font-semibold'>₹ {product.new_price}</div>
-                      <div className='mt-1 text-gray-400 text-sm line-through'>₹ {product.old_price}</div>
+                      <div className="font-bold text-lg truncate">{product.name}</div>
+                      <div className="text-green-600 text-md font-semibold">₹ {product.new_price}</div>
+                      <div className="text-gray-400 text-sm line-through">₹ {product.old_price}</div>
                     </div>
                   </Link>
                 </div>
@@ -145,41 +142,38 @@ const Home = () => {
             </div>
           )}
 
-          <div className="mt-10 md:mt-24">
-            <Link to='/mens'>
-              <button className='text-white bg-black p-3 md:p-5 px-6 md:px-8'>
-                See More
-              </button>
+          <div className="mt-6 md:mt-10">
+            <Link to="/mens">
+              <button className="bg-black text-white py-2 px-6 rounded-md">See More</button>
             </Link>
           </div>
         </div>
 
         {/* Kids Section */}
-        <div className='text-center place-content-center'>
-          <h1 className='text-4xl mt-3'>Champ Collection</h1>
-
+        <div className="text-center mt-10">
+          <h1 className="text-2xl md:text-4xl">Champ Collection</h1>
           {loadingKids ? (
-            <Loader /> // Show loader when data is loading
+            <Loader />
           ) : (
-            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6 ml-2'>
-              {products.filter((item) => item.category === "kid" || item.category === "kids").slice(0, 4).map((product) => (
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mx-4 lg:mx-0">
+              {products.filter((item) => item.category === 'kid' || item.category === 'kids').slice(0, 4).map((product) => (
                 <div
                   key={product.id}
-                  className='w-64 h-80 my-5 mx-auto border border-gray-200 rounded-lg text-left shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out'
+                  className="border border-gray-200 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
                 >
                   <Link to={`/${product.name}`} key={product.id}>
-                    <div className='overflow-hidden h-48'>
+                    <div className="overflow-hidden h-48">
                       <img
                         src={product.image}
                         onClick={() => SinglePage(product)}
-                        alt="product image"
-                        className='w-full h-full object-cover transform transition-transform duration-500 ease-in-out hover:scale-110 cursor-pointer'
+                        alt={product.name}
+                        className="w-full h-full object-cover transform transition-transform duration-500 ease-in-out hover:scale-110 cursor-pointer"
                       />
                     </div>
                     <div className="p-4">
-                      <div className='font-bold text-lg text-gray-800 truncate'>{product.name}</div>
-                      <div className='mt-1 text-lg text-green-600 font-semibold'>₹ {product.new_price}</div>
-                      <div className='mt-1 text-gray-400 text-sm line-through'>₹ {product.old_price}</div>
+                      <div className="font-bold text-lg truncate">{product.name}</div>
+                      <div className="text-green-600 text-md font-semibold">₹ {product.new_price}</div>
+                      <div className="text-gray-400 text-sm line-through">₹ {product.old_price}</div>
                     </div>
                   </Link>
                 </div>
@@ -187,41 +181,38 @@ const Home = () => {
             </div>
           )}
 
-          <div className="mt-24">
-            <Link to='/kids'>
-              <button className='text-white bg-black p-5 px-8'>
-                See More
-              </button>
+          <div className="mt-6 md:mt-10">
+            <Link to="/kids">
+              <button className="bg-black text-white py-2 px-6 rounded-md">See More</button>
             </Link>
           </div>
         </div>
 
         {/* Women's Section */}
-        <div className='text-center'>
-          <h1 className='text-4xl mt-3'>Queens Collection</h1>
-
+        <div className="text-center mt-10">
+          <h1 className="text-2xl md:text-4xl">Queens Collection</h1>
           {loadingWomen ? (
-            <Loader /> // Show loader when data is loading
+            <Loader />
           ) : (
-            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6 m-2'>
-              {products.filter((item) => item.category === "women" || item.category === "womens").slice(0, 8).map((product) => (
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mx-4 lg:mx-0">
+              {products.filter((item) => item.category === 'women' || item.category === 'womens').slice(0, 8).map((product) => (
                 <div
                   key={product.id}
-                  className='w-64 h-80 my-5 mx-auto border border-gray-200 rounded-lg text-left shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out'
+                  className="border border-gray-200 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
                 >
                   <Link to={`/${product.name}`} key={product.id}>
-                    <div className='overflow-hidden h-48 mb-3'>
+                    <div className="overflow-hidden h-48">
                       <img
                         src={product.image}
                         onClick={() => SinglePage(product)}
-                        alt="product"
-                        className='w-full h-full object-cover transform transition-transform duration-500 ease-in-out hover:scale-110 cursor-pointer'
+                        alt={product.name}
+                        className="w-full h-full object-cover transform transition-transform duration-500 ease-in-out hover:scale-110 cursor-pointer"
                       />
                     </div>
                     <div className="p-4">
-                      <div className='font-bold text-lg text-gray-800 truncate'>{product.name}</div>
-                      <div className='mt-1 text-lg text-green-600 font-semibold'>₹ {product.new_price}</div>
-                      <div className='mt-1 text-gray-400 text-sm line-through'>₹ {product.old_price}</div>
+                      <div className="font-bold text-lg truncate">{product.name}</div>
+                      <div className="text-green-600 text-md font-semibold">₹ {product.new_price}</div>
+                      <div className="text-gray-400 text-sm line-through">₹ {product.old_price}</div>
                     </div>
                   </Link>
                 </div>
@@ -229,23 +220,21 @@ const Home = () => {
             </div>
           )}
 
-          <div className="mt-24">
-            <Link to='/womens'>
-              <button className='text-white bg-black p-5 px-8'>
-                See More
-              </button>
+          <div className="mt-6 md:mt-10">
+            <Link to="/womens">
+              <button className="bg-black text-white py-2 px-6 rounded-md">See More</button>
             </Link>
           </div>
         </div>
 
-        <div className='mb-1'>
-          <img src={ad6} alt="" className='my-1' />
-          <div className='grid grid-cols-2'>
-            <img src={ad7} alt="" />
-            <img src={ad8} alt="" />
+        {/* Ads Section */}
+        <div className="my-10">
+          <img src={ad6} alt="ad" className="w-full mb-2" />
+          <div className="grid grid-cols-2 gap-4">
+            <img src={ad7} alt="ad" className="w-full" />
+            <img src={ad8} alt="ad" className="w-full" />
           </div>
         </div>
-
       </div>
     </>
   );
